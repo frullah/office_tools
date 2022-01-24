9c3cbd7f0827cc464e4a612944f52de468ddf9a0
Fajarullah <frullah12@gmail.com>
2022-01-24 11:10:37 +0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in office_tools.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/office_tools.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "office_tools/version"
+
+module OfficeTools
+  class Error < StandardError; end
+  # Your code goes here...
+end
